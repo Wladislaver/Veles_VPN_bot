2a106475c1400cc1a65eb8f308cfe2fa3dc9e31c
Wladislaver <Wladislaver@mail.ru>
2024-08-09 13:08:21 +0700

    no code

--- config.py
+++ config.py
@@ -20,11 +20,11 @@ class Config:
 
 config = Config(
     tg_bot=TgBotConfig(
-        token="7159548951:AAH-rQWNGzOjXL-Iw4chrzYm0Wp4yozyY5c",
+        token="7",
         # Идентификаторы администраторов бота
         admin_ids=[113931582]
     ),
     yoomoney=YooMoneyConfig(
-        token="381764678:TEST:83055"
+        token="3"
     )
 )
